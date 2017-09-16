18e82f7268013348222877593ba44db7fe29dcc7
Bart Wijnants <bartwijnants@outlook.com>
2017-09-16 12:02:34 +0200

    heroke fix

--- docker_travis_heroku.sh
+++ docker_travis_heroku.sh
@@ -4,5 +4,5 @@ docker build -t bartw/ploker:$TRAVIS_BUILD_NUMBER .
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 docker push bartw/ploker:$TRAVIS_BUILD_NUMBER
 docker login -u=$HEROKU_USERNAME -p=$HEROKU_AUTH_TOKEN registry.heroku.com;
-docker tag bartw/ploker:$TRAVIS_BUILD_NUMBER registry.heroku.com/ploker;
-docker push registry.heroku.com/ploker;
+docker tag bartw/ploker:$TRAVIS_BUILD_NUMBER registry.heroku.com/ploker/web;
+docker push registry.heroku.com/ploker/web;
